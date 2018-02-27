7130d2870a5f37258e2a5ee8fa818e27cfd03a0f
niqg <32465777+niqg@users.noreply.github.com>
2018-02-27 10:13:59 -0500

    Add files via upload

---
+++ List/ArrayList-Based/ListArrayListBased.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class ListArrayListBased implements ListInterface 
+{
+	protected ArrayList<Object> items;
+	
+	public ListArrayListBased()
+	{
+		items = new ArrayList <Object>();
+	}
+	
+	public ListArrayListBased(int i)
+	{
+		items = new ArrayList <Object>(i);
+	}
+	
+	@Override
+	public boolean isEmpty() 
+	{
+		return (items.size() == 0 ? true : false);	
+	}
+
+	@Override
+	public int size() 
+	{
+		
+		return items.size();
+	}
+
+	@Override
+	public void add(int index, Object item) throws ListIndexOutOfBoundsException 
+	{
+		if(index <= size() && index >= 0)
+			items.add(index, item);
+
+		else
+			throw new ListIndexOutOfBoundsException("Index Out Of Bounds Exception on add");
+		
+	}
+
+	@Override
+	public Object get(int index) throws ListIndexOutOfBoundsException 
+	{
+		if(index < size() && index >= 0)
+			return items.get(index);
+		else
+			throw new ListIndexOutOfBoundsException("Index Out Of Bounds Exception on get");
+		
+	}
+
+	@Override
+	public void remove(int index) throws ListIndexOutOfBoundsException 
+	{
+		if(index < size() && index >= 0)
+		{
+			items.remove(index);
+		}
+		
+		else
+			throw new ListIndexOutOfBoundsException("Index Out Of Bounds Exception on remove");
+		
+	}
+
+	@Override
+	public void removeAll() 
+	{
+		items = new ArrayList<Object>();
+	}
+
+}
---
+++ List/ArrayList-Based/ListArrayListBasedPlus.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class ListArrayListBasedPlus extends ListArrayListBased
+{
+	@Override
+	public String toString()
+	{
+		String s = "";
+		
+		int size = size();
+		for(int i = 0 ; i < size ; i++)
+		{
+			s += items.get(i) + " ";
+		}
+		return s;
+	}
+	
+	public void reverse()
+	{
+		int mid = size()/2;
+		int j = size() -1;
+		
+		for (int i = 0 ; i < mid ; i++)
+		{
+			Object temp = super.get(i);
+			
+			items.remove(i);
+			items.add(i, items.get(j - 1));
+			items.remove(j);
+			items.add(j, temp);
+				
+		}
+		
+		
+	}
+}
+
